0f9aeb231a3c1f9d8f25ccfb1415c02e2e973d0f
ugly <ugly@MacBook-Pro-Mihail.local>
2025-03-04 17:08:56 +0300

    change: custom loading page

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,8 +1,9 @@
-import { Icon, Logo } from '@lobehub/ui';
+import { Icon } from '@lobehub/ui';
 import { Loader2 } from 'lucide-react';
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Center, Flexbox } from 'react-layout-kit';
+import SndiLogo from '../Logo/SndiLogo';
 
 const Loading = memo(() => {
   const { t } = useTranslation();
@@ -10,7 +11,7 @@ const Loading = memo(() => {
   return (
     <Flexbox height={'100vh'} width={'100%'}>
       <Center flex={1} gap={12} width={'100%'}>
-        <Logo extra={'SD'} size={48} type={'combine'} />
+        <SndiLogo size={48}/>
         <Center gap={16} horizontal>
           <Icon icon={Loader2} spin />
           {t('custom.initializing')}
